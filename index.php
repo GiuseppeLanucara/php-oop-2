ec4cf5ff1eb17ef9ceecceba60acc41bd9e030aa
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-12-12 17:25:03 +0100

    prova

--- index.php
+++ index.php
@@ -14,9 +14,44 @@
     ];
 
     $generals = [
-        $croccantini = new Product(),
+        $croccantini = new Product("/img/foodan.jpg", "Croccantini", "HappyDog", "Cibo per Cani", 15, new Category('Cani')),
         var_dump($croccantini),
 
-        $cuccia = new Product(),
+        $cuccia = new Product("/img/cat.jpg", "Culla", "HappyCat", "Culla per Gatti", 50, new Category('Gatti')),
         var_dump($cuccia),
     ];
+
+    ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Link BOOTSTRAP -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+     <title>Php-OOP-2</title>
+ </head>
+
+ <body>
+     <div class="container">
+         <div class="row row-cols-3 g-3 text-center">
+             <?php foreach ($products as $product) { ?>
+                 <div class="col">
+                     <div class="card">
+                         <div class="card-body">
+                             <img src="<?php echo $product->image ?>" alt="">
+                             <h2 class="card-title"><?php echo $product->title ?></h2>
+                             <p class="card-text">Price: <?php echo $product->price ?></p>
+                             <p class="card-text">Category: <?php echo $prod->categories->type ?></p>
+                         </div>
+                     </div>
+                 </div>
+             <?php } ?>
+         </div>
+     </div>
+ </body>
+
+ </html>
